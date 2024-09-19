3f8f9124ad38b1fc8b43e9a10080239a88c70255
Juergen 'eTM' Mangler <juergen.mangler@gmail.com>
2024-09-19 16:52:20 +0200

    error handling

--- lib/cpee-eval-ruby/implementation.rb
+++ lib/cpee-eval-ruby/implementation.rb
@@ -33,7 +33,29 @@ module CPEE
 
     class DoIt < Riddl::Implementation #{{{
       def exec(__struct,__code,result=nil,headers=nil)
-        __struct.instance_eval(__code)
+        __ret = {}
+        __cat = catch WEEL::Signal::Again do
+          __ret[:res] = __struct.instance_eval(__code)
+          WEEL::Signal::Proceed
+        end
+        if __cat.nil? || __cat == WEEL::Signal::Again
+          __ret[:signal] << 'Signal::Again'
+        end
+      rescue => err
+        __ret[:signal] = 'Signal::Error'
+        __ret[:signal_text] = (err.backtrace ? err.backtrace[0].gsub(/([\w -_]+):(\d+):in.*/,'\\1, Line \2: ') : '') + err.message
+      rescue WEEL::Signal::Again
+        __ret[:signal] = 'Signal::Again'
+      rescue  WEEL::Signal::Error => err
+        __ret[:signal] = 'Signal::Error'
+        __ret[:signal_text] = (err.backtrace ? err.backtrace[0].gsub(/([\w -_]+):(\d+):in.*/,'\\1, Line \2: ') : '') + err.message
+      rescue WEEL::Signal::Stop
+        __ret[:signal] = 'Signal::Stop'
+      rescue SyntaxError => err
+        __ret[:signal] = 'Signal::SyntaxError'
+        __ret[:signal_text] = err.message
+      ensure
+        return __ret
       end
 
       def response
@@ -49,9 +71,11 @@ module CPEE
         call_result = JSON::parse(@p.shift.value.read) if @p.any? && @p[0].name == 'call_result'
         call_headers = JSON::parse(@p.shift.value.read) if @p.any? && @p[0].name == 'call_headers'
 
+        local = local[0] if local && local.is_a?(Array)
+
         # symbolize keys, because JSON
         dataelements.transform_keys!{|k| k.to_sym}
-        local.first.transform_keys!{|k| k.to_sym} if local
+        local.transform_keys!{|k| k.to_sym} if local
         endpoints.transform_keys!{|k| k.to_sym}
         additional.transform_keys!{|k| k.to_sym}
         additional.each_value do |v|
@@ -60,32 +84,45 @@ module CPEE
 
         if status || call_result || call_headers
           struct = WEEL::ManipulateStructure.new(dataelements,endpoints,status,local,additional)
-          exec struct, code, CPEE::EvalRuby::Translation::simplify_structurized_result(call_result), call_headers
-          ret = []
-          ret.push Riddl::Parameter::Simple.new('result','')
+          execresult = exec struct, code, CPEE::EvalRuby::Translation::simplify_structurized_result(call_result), call_headers
+
+          send = []
+          send << Riddl::Parameter::Simple.new('result',execresult[:res])
+          if execresult[:signal]
+            send << Riddl::Parameter::Simple.new('signal',execresult[:signal])
+            send << Riddl::Parameter::Simple.new('signal_text',execresult[:signal_text] || '')
+            @status = 555
+          end
 
           res = {}
           struct.changed_data.each do |e|
             res[e] = struct.data[e]
           end
           if res.any?
-            ret.push Riddl::Parameter::Complex.new('dataelements','application/json',JSON::generate(data)) if mode == :full
-            ret.push Riddl::Parameter::Complex.new('changed_dataelements','application/json',JSON::generate(res))
+            send << Riddl::Parameter::Complex.new('dataelements','application/json',JSON::generate(data)) if mode == :full
+            send << Riddl::Parameter::Complex.new('changed_dataelements','application/json',JSON::generate(res))
           end
           res = {}
           struct.changed_endpoints.each do |e|
             res[e] = struct.endpoints[e]
           end
           if res.any?
-            ret.push Riddl::Parameter::Complex.new('endpoints','application/json',JSON::generate(endpoints)) if mode == :full
-            ret.push Riddl::Parameter::Complex.new('changed_endpoints','application/json',JSON::generate(res))
+            send << Riddl::Parameter::Complex.new('endpoints','application/json',JSON::generate(endpoints)) if mode == :full
+            send << Riddl::Parameter::Complex.new('changed_endpoints','application/json',JSON::generate(res))
           end
-          ret.push Riddl::Parameter::Complex.new('changed_status','application/json',JSON::generate(status)) if struct.changed_status
-          ret
+          send << Riddl::Parameter::Complex.new('changed_status','application/json',JSON::generate(status)) if struct.changed_status
+          send
         else
           struct = WEEL::ReadStructure.new(dataelements,endpoints,local,additional)
-          res = exec struct, code
-          Riddl::Parameter::Simple.new('result',res)
+          execresult = exec struct, code
+          send = []
+          send << Riddl::Parameter::Simple.new('result',execresult[:res])
+          if execresult[:signal]
+            send << Riddl::Parameter::Simple.new('signal',execresult[:signal])
+            send << Riddl::Parameter::Simple.new('signal_text',execresult[:signal_text] || '')
+            @status = 555
+          end
+          send
         end
       end
     end #}}}
